31b3a5c6da2748d132fa709e362dc1e3b3e9e493
Dasha Lary <dk2800@nyu.edu>
2020-09-24 18:38:04 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,10 +1,10 @@
 # require libraries/modules here
 require 'nokogiri'
 require 'pry'
-
+html = File.read('fixtures/kickstarter.html')
+kickstarter = Nokogiri::HTML(html)
 def create_project_hash
-# html = File.read('fixtures/kickstarter.html')
-# kickstarter = Nokogiri::HTML(html)
+
 projects = {}
 kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
